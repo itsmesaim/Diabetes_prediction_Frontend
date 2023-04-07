a144cfb8e5241aed501af1821b6d3680f9626c46
itsmesaim <saimkaskar69@gmail.com>
2023-04-08 00:03:08 +0530

    implemented the logics

--- script.js
+++ script.js
@@ -90,22 +90,152 @@ function back(x, event) {
 
 // logic
 let thickness = null;
+let pregnancy;
+let bloodpressure;
+let glucose = null;
+let insulin;
+let pedigree;
+let age;
+let BMI;
 
 function tricepThickness() {
-	const options = document.getElementsByName('1');
-	for (let i = 0; i < options.length; i++) {
-		if (options[i].checked) {
-			const valueRange = options[i].value.split('-');
-			const min = parseInt(valueRange[0]);
-			const max = parseInt(valueRange[1]);
-			thickness = Math.floor(Math.random() * (max - min + 1) + min);
-			break;
-		}
-	}
-	if (thickness !== null) {
-		// Use the selected thickness as needed
-		console.log(`Selected tricep thickness: ${thickness}`);
-	} else {
-		alert('Please select an option');
-	}
+    const options = document.getElementsByName('1');
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].checked) {
+            const valueRange = options[i].value.split('-');
+            const min = parseInt(valueRange[0]);
+            const max = parseInt(valueRange[1]);
+            thickness = Math.floor(Math.random() * (max - min + 1) + min);
+            break;
+        }
+    }
+    if (thickness !== null) {
+        // Use the selected thickness as needed
+        console.log(`Selected tricep thickness: ${thickness}`);
+    } else {
+        alert('Please select an option');
+    }
+}
+
+
+function bloodPressure() {
+    let option1 = document.getElementById("pressure1");
+    let option2 = document.getElementById("pressure2");
+
+    if (option1.checked) {
+        bloodpressure = Math.floor(Math.random() * (130 - 80 + 1)) + 80;
+    } else if (option2.checked) {
+        bloodpressure = Math.floor(Math.random() * (80 - 50 + 1)) + 50;
+    }
+}
+
+
+
+
+
+function glucoseLevel() {
+    let option1 = document.getElementById("glucose1");
+    let option2 = document.getElementById("glucose2");
+    let option3 = document.getElementById("glucose3");
+    let option4 = document.getElementById("glucose4");
+
+    if (option1.checked) {
+        glucose = Math.floor(Math.random() * (180 - 140 + 1)) + 140;
+    } else if (option2.checked) {
+        glucose = Math.floor(Math.random() * (160 - 120 + 1)) + 120;
+    } else if (option3.checked) {
+        glucose = Math.floor(Math.random() * (140 - 100 + 1)) + 100;
+    } else if (option4.checked) {
+        glucose = Math.floor(Math.random() * (120 - 80 + 1)) + 80;
+    }
+}
+
+
+function insulinDose() {
+    let radios = document.getElementsByName('4');
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            let value = 0;
+            switch (radios[i].value) {
+                case '0-5':
+                    value = Math.floor(Math.random() * 6);
+                    break;
+                case '5-10':
+                    value = Math.floor(Math.random() * 6) + 5;
+                    break;
+                case '10-15':
+                    value = Math.floor(Math.random() * 6) + 10;
+                    break;
+                case '15-20':
+                    value = Math.floor(Math.random() * 6) + 15;
+                    break;
+                default:
+                    value = 0;
+                    break;
+            }
+            insulin = value;
+            break;
+        }
+    }
+}
+
+
+
+function calculatePedigree() {
+    let radios = document.getElementsByName('5');
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            let value = 0;
+            switch (radios[i].value) {
+                case '0':
+                    value = 0;
+                    break;
+                case '0.5-1.5':
+                    value = parseFloat((Math.random() * (1.5 - 0.5) + 0.5).toFixed(3));
+                    break;
+                case '1.5-2.5':
+                    value = parseFloat((Math.random() * (2.5 - 1.5) + 1.5).toFixed(3));
+                    break;
+                default:
+                    value = 0;
+                    break;
+            }
+            pedigree = value;
+            break;
+        }
+    }
+
+}
+
+
+function storeAge() {
+  let inputAge = document.getElementById("ageInput").value;
+  if (Number.isInteger(parseInt(inputAge))) {
+    age = parseInt(inputAge);
+  } else {
+    alert("Please enter a valid integer for age.");
+  }
+}
+
+
+
+function calculateBMI() {
+  const bmiInput = document.getElementById("bmi-input");
+  BMI = parseFloat(bmiInput.value);
+}
+
+function storePregnancy() {
+    pregnancy = parseInt(document.getElementById("pregnancy").value);
+}
+
+
+function MainFunction() {
+    tricepThickness();
+    bloodPressure();
+    glucoseLevel();
+    insulinDose();
+    calculatePedigree();
+    storeAge();
+    calculateBMI();
+    storePregnancy();
 }
